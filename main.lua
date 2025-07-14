f8d50e10784544a52d1551af1af031852b9bd769
Rustypredator <contact@rusty.info>
2025-07-14 09:55:43 +0200

    1st iteration of self update

--- main.lua
+++ main.lua
@@ -1,1 +1,72 @@
 -- VaultGuard Main Server Script
+
+local version = "0.0.1"
+
+-- Self Update function
+-- This function checks for updates to the main server script and updates it if a new version is available.
+-- @return int 0 if the update was successful, 1 if no update was needed, or -1 if there was an error.
+local function updateSelf()
+    local selfUpdateUrl = "https://raw.githubusercontent.com/Rustypredator/cc-vault-guard/refs/heads/main/main.lua"
+    local selfVersionUrl = "https://raw.githubusercontent.com/Rustypredator/cc-vault-guard/refs/heads/main/main.ver"
+    local response = http.get(selfVersionUrl)
+    if response then
+        local latestVersion = response.readAll()
+        response.close()
+        if latestVersion ~= version then
+            local updateResponse = http.get(selfUpdateUrl)
+            if updateResponse then
+                local file = fs.open("startup.lua", "w")
+                file.write(updateResponse.readAll())
+                file.close()
+                return 0  -- Update successful
+            else
+                return -1  -- Error downloading the update
+            end
+        else
+            return 1
+        end
+    else
+        return -1  -- Error checking for updates
+    end
+end
+
+local function init()
+    print(" -> Initializing VaultGuard Main Server Script " .. version .. "...")
+    -- do some checks:
+    if not http then
+        print("Error: HTTP API is not enabled.")
+        print("  -> Updates will not work")
+        print("  -> Please enable it in the settings.")
+        -- do not break, just warn
+        for i = 1, 5 do
+            print(".")
+            sleep(1)
+        end
+    else
+        print(" -> HTTP API")
+        print(" -> Checking for updates...")
+        local updateResult = updateSelf()
+        if updateResult == 0 then
+            print(" -> UPDATE SUCCESSFUL")
+            print(" -> REBOOTING")
+            sleep(2)
+            os.reboot()  -- Reboot to apply the update
+        elseif updateResult == 1 then
+            print(" -> UP TO DATE")
+        elseif updateResult == -1 then
+            print(" -> UPDATE FAILED")
+            sleep(2)
+        end
+    end
+    return true
+end
+
+local function main()
+    if not init() then
+        print("Initialization failed. Exiting...")
+        return
+    end
+
+    -- Main server logic goes here
+    -- For example, starting the server, handling requests, etc.
+end
